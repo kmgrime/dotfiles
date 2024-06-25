6495ac48be68ddd6ce0f9465665261e852fe52fa
Kjell-Magnus Grime <kjell-magnus.grime@statkraft.com>
2024-06-25 14:23:46 +0200

    add lsp-config update to nvim

---
+++ nvim/lua/plugins/lsp-config.lua
@@ -0,0 +1,44 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls", "bicep", "yamlls" }
+      })
+    end
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities
+      })
+
+      lspconfig.bicep.setup{
+        capabilities = capabilities,
+        cmd = { "bicep-lsp" },
+        filetypes = { "bicep" },
+        root_dir = require('lspconfig').util.root_pattern(".git", "main.bicep"),
+        settings = {},
+      }
+
+      lspconfig.yamlls.setup({
+        capabilities = capabilities
+      })
+
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+    end
+  }
+}
