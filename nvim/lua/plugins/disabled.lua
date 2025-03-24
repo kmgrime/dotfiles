eafcd41d18910f2a4d8be837c11bce674974b827
Kjell-Magnus Grime <kjell-magnus.grime@statkraft.com>
2025-03-24 13:33:50 +0100

    update nvim

---
+++ nvim/lua/plugins/disabled.lua
@@ -0,0 +1,9 @@
+return {
+  -- disable trouble
+  { "folke/noice.nvim", enabled = false },
+  { "rcarriga/nvim-notify", enabled = false },
+  { "folke/snacks.nvim", opts = {
+    dashboard = { enabled = false },
+  } },
+  { "folke/trouble.nvim", enabled = false },
+}
