a10c5ca71c34fd77bc1f646887a768700bef61b5
kmgrime <kmgrime@gmail.com>
2025-09-16 09:59:05 +0200

    update(fontinstaller): update version

--- scripts/fontinstaller.sh
+++ scripts/fontinstaller.sh
@@ -1,8 +1,8 @@
 #/bin/bash
 # install UbuntuMono Nerd Font --> u can choose another at: https://www.nerdfonts.com/font-downloads
 echo "[-] Download fonts [-]"
-echo "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/UbuntuMono.zip"
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/UbuntuMono.zip
+echo "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/UbuntuMono.zip"
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/UbuntuMono.zip
 unzip UbuntuMono.zip -d ~/.fonts
 fc-cache -fv
 echo "done!"
